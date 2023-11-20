48f62b7529067ea6be53fa4b2a322d5f6bd7e141
Mikko Anttila <fmian007@edu.xamk.fi>
2023-11-20 17:40:55 +0200

    no message

---
+++ Shaderpeli/Assets/Shaders/ShaderGraphs/SubGraph3Function.hlsl
@@ -0,0 +1,14 @@
+#ifndef CUSTOM_ADD_NODE_INCLUDED
+#define CUSTOM_ADD_NODE_INCLUDED
+
+void CustomAdd_Float(in float A, in float B, out float Result)
+{
+    Result = A + B;
+}
+
+void CustomAdd_half(in float A, in float B, out float Result)
+{
+    Result = A + B;
+}
+
+#endif
